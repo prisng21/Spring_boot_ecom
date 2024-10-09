e6e88a785d1cbd5d00fc3eaddd6976ddf374b921
prince singh <108452811+prisng21@users.noreply.github.com>
2024-10-10 00:43:32 +0600

    Working on Product Details, available product, out of stock products logic implemented. show Empty category with proper message etc13.

--- Ecommerce_Store/src/main/java/com/mdtalalwasim/ecommerce/controller/HomeViewController.java
+++ Ecommerce_Store/src/main/java/com/mdtalalwasim/ecommerce/controller/HomeViewController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mdtalalwasim.ecommerce.entity.Category;
@@ -52,9 +53,10 @@ public class HomeViewController {
 		return "product";
 	}	
 	
-	@GetMapping("/product")
-	public String viewProduct() {
-		
+	@GetMapping("/product/{id}")
+	public String viewProduct(@PathVariable long id, Model model) {
+		Product productById = productService.getProductById(id);
+		model.addAttribute("product",productById);
 		return "view-product";
 	}
 	
